0eda6a0a9ab64f8daad9adcb90aa448852158e46
YASHAVANTHAKUMARA <68936569+yash-arh@users.noreply.github.com>
2020-09-11 16:41:59 +0530

    Add files via upload
    Day 3-4 Js assignments

---
+++ Day 3-4 assignment/Question 1.js
@@ -0,0 +1,14 @@
+function firstRes()
+{
+    const element=document.getElementsByClassName("yash");
+    element[0].src ='https://wallpapercave.com/wp/wp5220297.jpg';
+}
+
+function secondRes() {
+    const element = document.getElementsByClassName("yash");
+    element[0].src = 'https://i.pinimg.com/736x/11/45/15/114515db31f948560fa06c8dcf885203.jpg';
+}
+function thirdRes() {
+    const element = document.getElementsByClassName("yash");
+    element[0].src ="https://i.pinimg.com/564x/e4/ac/df/e4acdf79a0dd1f0b25e4cbd8be912f0b.jpg";
+}
---
+++ Day 3-4 assignment/Question 3.js
@@ -0,0 +1,32 @@
+let criminals = [
+    {
+        name: "Yash",
+        age: "23",
+        country: "India",
+        hobbies: ['draw', 'dance', 'kill']
+    },
+    {
+        name: "Deeps",
+        age: "20",
+        country: "India",
+        hobbies: ['kill', 'hack', 'fight']
+    },
+    {
+        name: "edward",
+        age: "32",
+        country: "queens",
+        hobbies: ['sing', 'kill', 'write']
+    },
+
+    {
+        name: "charlie",
+        age: "38",
+        country: "roswell",
+        hobbies: ['kill', 'kill', 'write']
+    }
+];
+function display(criminals){
+    console.log(criminals);
+}
+
+display(criminals);
